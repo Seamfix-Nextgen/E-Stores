4bfa44c9080b1bf93672dcc42d4265f0443cde9e
Okoli kingsley <kingxleeya@gmail.com>
2022-10-18 12:36:03 +0100

    new

--- models/user.js
+++ models/user.js
@@ -1,95 +1,69 @@
-const { default: mongoose } = require("mongoose");
-const validator = require("validator");
+const mongoose = require ("mongoose");
+const express = require("express");
 const bcrypt = require("bcryptjs");
-const crypto = require("crypto");
+const validator = require("validator");
+const crypto = require ("crypto");
+const { stringify } = require("querystring");
 
-const userSchema = new mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: [true, "A user must have a name"],
-    },
-    email: {
-      type: String,
-      required: [true, "A user must have an email"],
-      unique: true,
-      validate: [validator.isEmail, "Please enter a valid email"],
-    },
-    password: {
-      type: String,
-      required: [true, "A user must have an password"],
-      select: false,
-      minLength: [8, "Password must ba at least 8 characters"],
-      validate: {
-        validator: function (val) {
-          return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])[A-Za-z\d]{8,}/.test(val);
-        },
+const userSchema = new mongoose.schema({
+
+fullName: {
+    type: stringify,
+    required:[true, "customer should have a name"],
+},
+
+email:{
+type: stringify,
+required:[true, "An email is required"],
+unique: true,
+validate: [validator.isEmail, "kindly enter a valid email"],
+},
+
+password:{
+    type: string,
+    required:[true, "user must have a password"],
+    select: false,
+    minLength:[8, "password should not exceed 8 characters"],
+    validate:{
+        validator: function (val){
+return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])[A-Za-z\d]{8,}/.test(val);
+},
         message:
-          "Password must contain at least a number, a lowercase and an uppercase alphabeth",
-      },
+        "Password must contain at least a number, a lowercase and an uppercase alphabeth",
     },
-    passwordConfirm: {
-      type: String,
-      required: [true, "A user must have an passwordConfirm"],
-      select: false,
-      validate: {
-        validator: function (val) {
-          return val === this.password;
-        },
-        message: "Password and confirm password are different",
-      },
-     
+},
+
+passwordConfirm:{
+    type: string,
+    required:[true, "user must have a comfirm password"],
+    select: false,
+    minLength:[8, "password should not exceed 8 characters"],
+    validate:{
+        validator: function (val){
+            return val===this.password;
+},
+message:"password and confirm password are different"
     },
-    role: {
-      type: String,
-      default: "user",
-      enum: ["user", "admin"],
+
+    role:{
+    type: string,
+    default: "user",
+    enum:["user", "admin"],
     },
-    passwordResetToken: String,
-    passwordChangedAt: Date,
-    passwordResetToken: String,
-    passwordTokenExpires: Date,
-    active: {
-      type: Boolean,
-      default: true,
-      select: false,
-    },
-  },
-  { toObject: { virtuals: true }, toJSON: { virtuals: true } }
+
+
+passwordResetToken: String,
+passwordChangedAt: Date,
+passwordResetToken: String,
+passwordTokenExpires: Date,
+
+active:{
+    type: Boolean,
+    default: true,
+    select:false,
+},
+},
+
+{ toObject:{virtuals:true}, toJSON:{virtuals:true} }
 );
 
-userSchema.pre("save", async function (next){
-if (!this.isModified("password")) {
-   return next();
-}
-let salt = await bcrypt.genSalt(10);
-this.password = await bcrypt.hash(this.password, salt);
-this.passwordConfirm = undefined;
-next();
-});
-
-userSchema.methods.createPasswordResetToken = function () {
-    const resetToken = crypto.randomBytes(32).toString("hex");
-  
-    this.passwordResetToken = crypto
-      .createHash("sha256")
-      .update(resetToken)
-      .digest("hex");
-    this.passwordTokenExpires = Date.now() + 10 * 60 * 1000;
-    return resetToken;
-  };
-  
-  userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
-    if (this.passwordChangedAt) {
-      console.log(JWTTimestamp < this.passwordChangedAt);
-      return JWTTimestamp < this.passwordChangedAt;
-    }
-    return false;
-  };
-  
-
-
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
