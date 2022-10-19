9b3f2e172204cdf98251254fa071bbf9a02bf617
Okoli kingsley <kingxleeya@gmail.com>
2022-10-19 11:28:15 +0100

    new

---
+++ routes/order-routes.js
@@ -0,0 +1,17 @@
+const express = require ("express")
+const{
+    protect,
+    restrictTo
+} = ("../controllers/auth-controllers");
+
+const{
+    getOrder,
+    getAllOrder
+} = ("../controllers/order-controllers");
+const router = express.Router();
+
+
+router.get("/", protect, restrictTo("admin"), getAllOrder);
+router.get("/:id", protect, getOrder)
+
+module.exports = router;
