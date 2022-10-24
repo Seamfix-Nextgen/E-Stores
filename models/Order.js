a198e87d04f6ea86a22c5e52057a245c6aef6887
p-agboola <peterolamilekan65@gmail.com>
2022-10-24 16:35:34 +0100

    cart

---
+++ models/Order.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    required: [true, "order must have userId"],
+    ref: "User",
+  },
+  cartId: [
+    {
+      type: mongoose.Schema.ObjectId,
+      required: [true, "cart id is required"],
+      ref: "Cart",
+    },
+  ],
+  totalAmount: {
+    type: Number,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
