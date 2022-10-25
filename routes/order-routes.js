d68fc7aafd84692212e09af525f4a44a8c923203
p-agboola <peterolamilekan65@gmail.com>
2022-10-25 21:23:57 +0100

    routes

--- routes/order-routes.js
+++ routes/order-routes.js
@@ -1,17 +1,10 @@
-const express = require ("express")
-const{
-    protect,
-    restrictTo
-} = ("../controllers/auth-controllers");
+const express = require("express");
+const { protect, restrictTo } = "../controllers/auth-controllers";
 
-const{
-    getOrder,
-    getAllOrder
-} = ("../controllers/order-controllers");
+const { getOrder, getAllOrder } = "../controllers/order-controllers";
 const router = express.Router();
 
-
 router.get("/", protect, restrictTo("admin"), getAllOrder);
-router.get("/:id", protect, getOrder)
+router.get("/:id", protect, getOrder);
 
 module.exports = router;
