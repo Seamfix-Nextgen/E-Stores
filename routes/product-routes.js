6d956268d7416469c639b7f62c109795301f0ca5
p-agboola <peterolamilekan65@gmail.com>
2022-10-27 14:13:27 +0100

    user

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const { protect, 
-  restrictTo 
-} = require("../controllers/auth-controller");
+const { protect, restrictTo } = require("../controllers/auth-contoller");
 const {
   createProduct,
   listCategories,
@@ -14,12 +12,15 @@ const {
 } = require("../controllers/product-controller");
 const router = express.Router();
 
-router.route("/").post(protect,restrictTo("admin"), 
-createProduct)
-.get(listAllProduct);
-router.route("/:id").delete(protect, restrictTo("admin"), deleteProduct)
-  .patch(protect,restrictTo("admin"), updateProduct)
-.get(productById);
+router
+  .route("/")
+  .post(protect, restrictTo("admin"), createProduct)
+  .get(listAllProduct);
+router
+  .route("/:id")
+  .delete(protect, restrictTo("admin"), deleteProduct)
+  .patch(protect, restrictTo("admin"), updateProduct)
+  .get(productById);
 router.route("/:category").get(listCategories);
 router.route("/:shop").get(listProductByShop);
 router.route("/:latest").get(listByLatest);
