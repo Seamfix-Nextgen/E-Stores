4b6169030ac8d5bc091cd7e21ce20fcd0e932f0b
Okoli kingsley <kingxleeya@gmail.com>
2022-10-27 11:19:27 +0100

    Update

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,21 +1,27 @@
-const {
-  createAProduct,
-  deleteProduct,
-  listByCategories,
-  listproductByShop,
-  productByID,
-  updateProduct,
-  listAllProducts,
-  listbyLatest,
-} = require("../controllers/product-controller");
-
 const express = require("express");
-
+const { protect, 
+  restrictTo 
+} = require("../controllers/auth-controller");
+const {
+  createProduct,
+  listCategories,
+  listProductByShop,
+  deleteProduct,
+  productById,
+  updateProduct,
+  listAllProduct,
+  listByLatest,
+} = require("../controllers/product-controller");
 const router = express.Router();
 
-router.route("/").post(createAProduct).get(listAllProducts);
-router.route("/:productID").delete(deleteProduct).patch(updateProduct).get(productByID);
-router.route("/:category").get(listByCategories);
-router.route("/:shop").get(listproductByShop);
-router.route("/latest").get(listbyLatest);
+router.route("/").post(protect,restrictTo("admin"), 
+createProduct)
+.get(listAllProduct);
+router.route("/:id").delete(protect, restrictTo("admin"), deleteProduct)
+  .patch(protect,restrictTo("admin"), updateProduct)
+.get(productById);
+router.route("/:category").get(listCategories);
+router.route("/:shop").get(listProductByShop);
+router.route("/:latest").get(listByLatest);
 
+module.exports = router;
